266963a390fe65d3543525968ad7edb65274fff5
Jesus D Santisteban <jesus.santisteban@gmail.com>
2023-08-11 04:55:07 +0000

    excuse generator complete

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function perfectExcuse() {
+  let randomExcuse = who[Math.floor(Math.random() * who.length)] + " ";
+  randomExcuse += action[Math.floor(Math.random() * action.length)] + " ";
+  randomExcuse += what[Math.floor(Math.random() * what.length)] + " ";
+  randomExcuse += when[Math.floor(Math.random() * when.length)] + " ";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  document.getElementById("excuse").innerHTML = randomExcuse;
+}
