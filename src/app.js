f66d065cd269866b89bb7d9e9f8d69449c1b9e31
Jesus D Santisteban <jesus.santisteban@gmail.com>
2023-09-05 00:40:45 +0000

    Upload final revision

--- src/app.js
+++ src/app.js
@@ -1,19 +1,26 @@
-let who = ["The dog", "My grandma", "His turtle", "My bird"];
-let action = ["ate", "peed", "crushed", "broke"];
-let what = ["my homework", "the keys", "the car"];
-let when = [
-  "before the class",
-  "right on time",
-  "when I finished",
-  "during my lunch",
-  "while I was praying"
-];
+/* eslint-disable */
+import "bootstrap";
+import "./style.css";
 
-function perfectExcuse() {
+import "./assets/img/rigo-baby.jpg";
+import "./assets/img/4geeks.ico";
+
+window.onload = function() {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
   let randomExcuse = who[Math.floor(Math.random() * who.length)] + " ";
   randomExcuse += action[Math.floor(Math.random() * action.length)] + " ";
   randomExcuse += what[Math.floor(Math.random() * what.length)] + " ";
   randomExcuse += when[Math.floor(Math.random() * when.length)] + " ";
 
   document.getElementById("excuse").innerHTML = randomExcuse;
-}
+};
